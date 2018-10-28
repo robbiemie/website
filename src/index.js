1db263eeb13a852be3a53f00d5e3f3cc04b3d3a1
杨欣 <yangxin2@yy.com>
2018-10-28 14:57:31 +0800

    add: 首页完成，准备使用swiper进行翻页展示

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './module/App'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
 
-import 'swiper/dist/css/swiper.css'
-
-Vue.use(VueAwesomeSwiper /* { default global options } */)
-let app = new Vue({
+new Vue({ // eslint-disable-line
   el: '#app',
   render: h => h(App)
 })
-app.$mount()
--- src/module/App.vue
+++ src/module/App.vue
@@ -1,33 +1,79 @@
 <template>
   <div class="page" id="app">
-    <div class="page__top">
-      <SvgText/>
-    </div>
-    <div class="page__bottom">
-      <SvgLine/>
-      <div class="page__wrap">
-        <div class="page__arrow page__arrow--1"></div>
-        <div class="page__arrow page__arrow--2"></div>
-        <div class="page__arrow page__arrow--3"></div>
-      </div>
-    </div>
+    <swiper class="mySwiper" :options="swiperOption" ref="mySwiper" @touchmove="touchmove">
+      <!-- slides -->
+      <swiperSlide>
+        <section class="section section--1">
+        <div class="page__top">
+          <SvgText/>
+        </div>
+        <div class="page__bottom">
+          <SvgLine/>
+          <div class="page__wrap">
+            <div class="page__arrow page__arrow--1"></div>
+            <div class="page__arrow page__arrow--2"></div>
+            <div class="page__arrow page__arrow--3"></div>
+          </div>
+          <div class="page__content">
+            <div class="page__person"></div>
+            <div class="page__round"></div>
+          </div>
+        </div>
+      </section>
+      </swiperSlide>
+      <swiperSlide>
+        <section class="section section--2"></section>
+      </swiperSlide>
+    </swiper>
+
   </div>
 </template>
 
 <script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 import SvgText from '@/component/main/SvgText'
 import SvgLine from '@/component/main/SvgLine'
+
 export default {
   name: 'app',
   data () {
     return {
+      swiperOption: {
+        initialSlide: 0,
+        direction: 'vertical',
+        effect: 'flip',
+        on: {
+          touchMove: () => {
+            console.log('e')
+          }
+        }
+      }
+    }
+  },
+  directives: {
+    bind (el) {
+
     }
   },
   components: {
+    swiper,
+    swiperSlide,
     SvgText,
     SvgLine
   },
+  computed: {
+    swiper () {
+      return this.$refs.mySwiper.swiper
+    }
+  },
   mounted () {
+  },
+  methods: {
+    touchmove (e) {
+      console.log('e', e)
+    }
   }
 }
 </script>
@@ -37,23 +83,32 @@ export default {
 @keyframes scaleArrow {
   to {
     opacity: 0.8;
-    transform: scale(0.8);
+    transform: scale(0.85);
+  }
+}
+.mySwiper {
+  height: 100%;
+}
+.section {
+  height: 100%;
+  &--1 {
+    // transform: translate3d(0,-50%,0);
+  }
+  &--2 {
+    background: red;
   }
 }
 .page{
+  position: relative;
+  overflow: hidden;
+  background: #fff;
   &__top {
-    position: absolute;
-    left: 0;
-    top: 0;
     width: 100%;
     height: 50%;
     background: #000;
     background: #ce526f;
   }
   &__bottom{
-    position: absolute;
-    left: 0;
-    bottom: 0;
     width: 100%;
     height: 50%;
     background: #000;
@@ -82,6 +137,38 @@ export default {
     &--3 {
       transform: translate(0px, -24px) rotate(-45deg);
     }
+  }
+  &__content {
+    position: relative;
+  }
+  &__bottom:hover &__person{
+    transform: translateX(40px);
+    transition: transform 2s ease;
+  }
+  &__bottom:hover &__round{
+    transform: translateY(-40px);
+    transition: transform 2s ease;
+  }
+  &__person {
+    position: absolute;
+    width: 800px;
+    height: 600px;
+    top: 0;
+    left: 0;
+    transition: transform 2s ease;
+    background: url(~@/img/person.png) no-repeat;
+    background-size: contain;
+    z-index: 1;
+  }
+  &__round {
+    position: absolute;
+    left: 100px;
+    top: 20px;
+    width: 800px;
+    height: 600px;
+    transition: transform 2s ease;
+    background: url(~@/img/round.png) no-repeat;
+    background-size: contain;
   }
 }
 </style>
