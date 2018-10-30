3174378870dc7c0d9a6ae9b47e85344ed3b329d1
yangxin2 <yang657850144@qq.com>
2018-10-30 10:59:46 +0800

    add: portfolio 搭建完成

--- src/module/portfolio/index.vue
+++ src/module/portfolio/index.vue
@@ -8,24 +8,66 @@
       Selected front-end development projects.
     </div>
     <div class="portfolio__list">
-      <div class="portfolio__item">
+      <div class="portfolio__item portfolio__item">
         <div class="portfolio__item--wrap">
-
+          <div class="portfolio__item--common portfolio__item--1"></div>
+          <div class="portfolio__item--common portfolio__item--1"></div>
         </div>
       </div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
-      <div class="portfolio__item"></div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--2"></div>
+          <div class="portfolio__item--common portfolio__item--2"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--3"></div>
+          <div class="portfolio__item--common portfolio__item--3"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--4"></div>
+          <div class="portfolio__item--common portfolio__item--4"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--5"></div>
+          <div class="portfolio__item--common portfolio__item--5"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--6"></div>
+          <div class="portfolio__item--common portfolio__item--6"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--7"></div>
+          <div class="portfolio__item--common portfolio__item--7"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--8"></div>
+          <div class="portfolio__item--common portfolio__item--8"></div>
+        </div>
+      </div>
+      <div class="portfolio__item portfolio__item">
+        <div class="portfolio__item--wrap">
+          <div class="portfolio__item--common portfolio__item--9"></div>
+          <div class="portfolio__item--common portfolio__item--9"></div>
+        </div>
+      </div>
     </div>
   </section>
   <Menus/>
 </div>
 </template>
+
 <script>
 import Menus from '@/component/Menu'
 export default {
@@ -86,7 +128,95 @@ export default {
       height: 280px;
       border-radius: 20px;
       border: 4px solid #fff;
+      background: #ccc;
+      overflow: hidden;
     }
+    &--common {
+      transform: translateY(0);
+      transition: transform 1s ease-out;
+    }
+    &--1 {
+      height: 100%;
+      background: url(~@/img/portfolio/1.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--2 {
+      height: 100%;
+      background: url(~@/img/portfolio/2.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--3 {
+      height: 100%;
+      background: url(~@/img/portfolio/3.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--4 {
+      height: 100%;
+      background: url(~@/img/portfolio/4.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--5 {
+      height: 100%;
+      background: url(~@/img/portfolio/5.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--6 {
+      height: 100%;
+      background: url(~@/img/portfolio/6.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--7 {
+      height: 100%;
+      background: url(~@/img/portfolio/7.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--8 {
+      height: 100%;
+      background: url(~@/img/portfolio/8.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+    &--9 {
+      height: 100%;
+      background: url(~@/img/portfolio/9.svg) no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+  }
+  &__item:hover &__item--1 {
+    transform: translateY(-100%);
+    transition: transform 1s ease-in;
+  }
+  &__item:hover &__item--2 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--3 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--4 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--5 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--6 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--7 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--8 {
+    transform: translateY(-100%);
+  }
+  &__item:hover &__item--9 {
+    transform: translateY(-100%);
   }
 }
 </style>
