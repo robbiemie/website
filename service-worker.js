10363996425103529cfd3b14ff9db96dca822cab
yangoogle <yang657850144@gmail.com>
2019-03-12 16:33:26 +0800

    chore: 发布新版本

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,12 @@ let mainCacheFiles = [
   'index.html'
 ]
 
-let version = 'cache-v1'
+let version = 'cache-version1'
 
 // 缓存静态资源
 self.addEventListener('install', function (evt) {
   // 强制更新sw.js
+  console.log(`${version} installing...`)
   self.skipWaiting()
   evt.waitUntil(
     caches.open(version).then(function (cache) {
@@ -16,12 +17,21 @@ self.addEventListener('install', function (evt) {
 })
 
 // 缓存更新
-self.addEventListener('active', function (evt) {
+self.addEventListener('activate', function (evt) {
+  console.log(`${version} activating...`)
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => {
+      console.log('client', client)
+      client.postMessage({ type: 'update', msg: '' })
+    })
+  })
   evt.waitUntil(
     caches.keys().then(function (cacheNames) {
+      console.log('cacheNames', cacheNames)
       return Promise.all(
         cacheNames.map(function (cacheName) {
           if (cacheName !== version) {
+            // 删除无效 cacheName
             return caches.delete(cacheName)
           }
         })
@@ -32,20 +42,22 @@ self.addEventListener('active', function (evt) {
 
 // 请求拦截
 self.addEventListener('fetch', function (evt) {
-  console.log('处理fetch事件:', evt.request.url)
+  // console.log('处理fetch事件:', evt.request.url)
   if (evt.request.url.match('sockjs')) return
   evt.respondWith(
     caches.match(evt.request).then(function (response) {
       if (response) {
         console.log('缓存匹配到res:', response)
-        return response
+        // return response
       }
-      console.log('缓存未匹配对应request,准备从network获取', caches)
+      // console.log('缓存未匹配对应request,准备从network获取', caches)
       return fetch(evt.request).then(function (response) {
-        console.log('fetch获取到的response:', response)
-        // caches.open(version).then(function (cache) {
-        //   cache.put(evt.request, response)
-        // })
+        console.log('req', evt.request, response.clone())
+        let cpResponse = response.clone()
+        // console.log('fetch获取到的response:', response)
+        caches.open(version).then(function (cache) {
+          cache.put(evt.request, cpResponse)
+        })
         return response
       })
     }).catch(function (err) {
---
+++ sw-register.js
@@ -0,0 +1,29 @@
+if (navigator.serviceWorker) {
+  console.log('当前环境支持serviceworker')
+  navigator.serviceWorker.addEventListener('message', function (e) {
+    // if(e.data==='sw')
+    console.log('重新加载页面', e.data)
+    location.reload()
+  })
+  navigator.serviceWorker.register('service-worker.js?v=cache-v1', { scope: './' }).then(registration => {
+    console.log('注册成功')
+    let serviceWorker
+    if (registration.installing) {
+      serviceWorker = registration.installing
+      console.log('当前状态为: installing', serviceWorker)
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting
+      console.log('当前状态为: waiting', serviceWorker)
+    } else if (registration.activating) {
+      serviceWorker = registration.activating
+      console.log('当前状态为: activating', serviceWorker)
+    }
+    if (serviceWorker) {
+      serviceWorker.addEventListener('statechange', e => {
+        console.log('当前状态变化为: ', e.target.state)
+      })
+    }
+  }).catch(err => {
+    console.log('注册失败', err)
+  })
+}
