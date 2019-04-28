511b474d7d09ba55a6e05b51dd1023216df4ad3e
yangoogle <yang657850144@gmail.com>
2019-04-28 11:02:34 +0800

    fix: 修改离线方案

--- src/static/sw-register.js
+++ src/static/sw-register.js
@@ -1,29 +1,36 @@
-if (navigator.serviceWorker) {
-  console.log('当前环境支持serviceworker')
+if ('serviceWorker' in navigator) {
+  // 检测serviceworker支持情况
   navigator.serviceWorker.addEventListener('message', function (e) {
     // if(e.data==='sw')
     console.log('重新加载页面', e.data)
     location.reload()
   })
-  navigator.serviceWorker.register('service-worker.js?v=cache-v1', { scope: './' }).then(registration => {
-    console.log('注册成功')
-    let serviceWorker
-    if (registration.installing) {
-      serviceWorker = registration.installing
-      console.log('当前状态为: installing', serviceWorker)
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting
-      console.log('当前状态为: waiting', serviceWorker)
-    } else if (registration.activating) {
-      serviceWorker = registration.activating
-      console.log('当前状态为: activating', serviceWorker)
-    }
-    if (serviceWorker) {
-      serviceWorker.addEventListener('statechange', e => {
-        console.log('当前状态变化为: ', e.target.state)
-      })
-    }
-  }).catch(err => {
-    console.log('注册失败', err)
-  })
+  let version = 1.0
+  let projName = 'website'
+  // if (projName.indexOf('/') === 0) {
+  //   projName = projName.slice(projName.indexOf('/') + 1)
+  // }
+  navigator.serviceWorker.register(`sw.js?p=${projName}&v=${version}`, { scope: './' })
+    .then(function (registration) {
+      console.log('serviceworker 注册成功')
+      let serviceworker
+      if (registration.installing) {
+        serviceworker = registration.installing
+        console.log('当前状态为： installing')
+      } else if (registration.waiting) {
+        serviceworker = registration.waiting
+        console.log('当前状态为： waiting')
+      } else if (registration.activing) {
+        serviceworker = registration.waiting
+        console.log('当前状态为： activing')
+      }
+      if (serviceworker) {
+        console.log('当前serviceworker', serviceworker)
+        serviceworker.addEventListener('statechange', function (event) {
+          console.log('当前状态改变为:', event.target.state)
+        })
+      }
+    }).catch(function (err) {
+      console.log('注册失败', err)
+    })
 }
